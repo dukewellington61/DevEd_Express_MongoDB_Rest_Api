caf33d33f2dab35158c3f2e35ef084b01dad694b
akoeth-portfolio <dukewellington61@googlemail.com>
2020-04-01 22:33:41 +0200

    1st commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+
+// IMPORT ROUTES
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('We are on home');
+});
+
+
+
+// Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECTION, 
+    { 
+        useUnifiedTopology: true, 
+        useNewUrlParser: true
+    }, 
+    () => console.log('connected to DB')
+);
+
+
+
+app.listen(3000);
