df916f2f09505c255d724dbc6b0d58cc9889fc54
akoeth-portfolio <dukewellington61@googlemail.com>
2020-04-02 23:29:25 +0200

    2nd commit

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
+// Middlewars
+app.use(bodyParser.json());
+app.use(cors()); /* https://www.youtube.com/watch?v=vjf774RKrLc&t=105s */
 
-// IMPORT ROUTES
+
+
+// Import Routes
 const postsRoute = require('./routes/posts');
 
 app.use('/posts', postsRoute);
@@ -13,8 +20,6 @@ app.use('/posts', postsRoute);
 app.get('/', (req, res) => {
     res.send('We are on home');
 });
-
-
 
 // Connect to DB
 mongoose.connect(
